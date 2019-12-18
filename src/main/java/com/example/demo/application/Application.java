c18e5676f71ef32e10610f3afa4f83b03452447f
Administrator <3118068186@qq.com>
2019-12-18 17:36:29 +0800

    配置

--- src/main/java/com/example/demo/application/Application.java
+++ src/main/java/com/example/demo/application/Application.java
@@ -1,10 +1,10 @@
-package application;
+package com.example.demo.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "controller")
+@ComponentScan(basePackages = "com.example.demo.controller")
 @EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import com.example.demo.pojo.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@EnableAutoConfiguration
+public class MyController {
+    @RequestMapping("/hello/{name}")
+    @ResponseBody
+    public String hello(@PathVariable(name = "name") String username) {
+        return "hello world!" + username;
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MyController.class, args);
+    }
+
+    @Autowired
+    HttpServletRequest request;
+    @RequestMapping("/testjsp")
+    public String tojsp(){
+        request.setAttribute("test","我的jsp测试！");
+        return "/index";
+    }
+
+    @RequestMapping("/thymeleaf")
+    public ModelAndView thymeleaf(){
+        ModelAndView mad = new ModelAndView();
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("apple",11.0f));
+        products.add(new Product("pear",11.0f));
+        products.add(new Product("banana",11.0f));
+        mad.addObject("allProducts",products);
+        mad.setViewName("/thymeleaf.html");
+        return mad;
+    }
+}
